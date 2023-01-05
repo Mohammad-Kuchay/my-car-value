0b41c62cca54d787ef085cdc1a678c491e973594
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-01-05 21:15:17 +0530

    adds cookie-session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    CookieSession({
+      keys: ['asdfasdf'],
+    }),
+  );
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // strip extra props
