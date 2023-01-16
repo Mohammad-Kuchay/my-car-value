519002f218078c3847c8bcf6f207047706ec1fe1
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-01-16 21:05:54 +0530

    sets the user.controller.spec.ts for testing

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './users.entity';
+import { retry } from 'rxjs';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'mnf@gmail.com',
+          password: 'asdf',
+        } as User);
+      },
+      find(email: string) {
+        return Promise.resolve([{ id: 1, email, password: 'asdf' } as User]);
+      },
+      // remove: () {},
+      // update: () {}
+    };
+    fakeAuthService = {
+      signin: () => {
+        return Promise.resolve({} as User);
+      },
+      signup: () => {
+        return Promise.resolve({} as User);
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
