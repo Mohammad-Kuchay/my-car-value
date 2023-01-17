01b6a059b58e8d75eef08590dbc1dbb1ebacc2ec
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-01-17 12:09:21 +0530

    adsss users.controller test cases

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,7 +3,6 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './users.entity';
-import { retry } from 'rxjs';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -26,8 +25,8 @@ describe('UsersController', () => {
       // update: () {}
     };
     fakeAuthService = {
-      signin: () => {
-        return Promise.resolve({} as User);
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
       },
       signup: () => {
         return Promise.resolve({} as User);
@@ -52,5 +51,35 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('mnf@gmail.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('mnf@gmail.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user user with given id is not found', async () => {
+    fakeUsersService.findOne = () => Promise.resolve(null);
+    try {
+      await controller.findUser('1');
+    } catch (error) {
+      expect(error.message).toMatch('user not found');
+    }
+  });
+
+  it('signin updates session object and retrun user', async () => {
+    const session = { userId: 0 };
+    const user = await controller.signin(
+      { email: 'mnf@gmail.com', password: '12345678' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
