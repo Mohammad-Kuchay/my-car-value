92a9e67cde88228bd5639b9d18abec6cd0af9e0a
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-01-25 12:53:45 +0530

    Finish experiment

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
-import { createReportDto } from './dtos';
+import { createReportDto, GetEstimateDto } from './dtos';
 import { Report } from './reports.entity';
 
 @Injectable()
@@ -27,5 +27,28 @@ export class ReportsService {
 
     report.approved = approved;
     return this.repo.save(report);
+  }
+
+  async createEstimate({
+    make,
+    model,
+    lng,
+    lat,
+    year,
+    mileage,
+  }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved Is TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawMany();
   }
 }
